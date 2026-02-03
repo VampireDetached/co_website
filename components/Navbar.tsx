c705907b45096ec89be807680e16f2b50ebc8b17
VampireDetached <1478173643@qq.com>
2026-02-03 15:51:33 +0800

    Modify website links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const navLinks = [
   { name: "Overview", href: "#overview", icon: false },
-  { name: "Paper", href: "#", icon: true },
-  { name: "GitHub", href: "https://github.com/XuanL17/HS_bench", icon: true},
-  { name: "Dataset", href: "#dataset", icon: false },
+  { name: "Paper", href: "https://arxiv.org/abs/2602.00685", icon: true },
+  { name: "GitHub", href: "https://github.com/XuanL17/HumanStudy-Bench", icon: true},
   { name: "Leaderboard", href: "#leaderboard", icon: false },
+  { name: "Dataset", href: "#dataset", icon: false },
 ];
 
 export default function Navbar() {
